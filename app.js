d74c2e93f1ed67d5bac76fc9d1c85d598054ac24
CodinJohn <johnathon.collado@gmail.com>
2024-06-09 15:44:06 -0400

    Worked with team to get this completed.

--- app.js
+++ app.js
@@ -1,83 +1,78 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
-let num1 = 0;
-let num2 = 0;
-let operator;
+let num1 = "";
+let num2 = "";
+let operator = "";
 let result = 0;
 
 /*------------------------ Cached Element References ------------------------*/
+
+const numberBtnEls = document.querySelectorAll('.number');
+const operatorBtnEls = document.querySelectorAll('.operator');
+const equalBtnEl = document.querySelector('.equals');
+const displayEl = document.querySelector('.display');
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
 
-const numberBtnEls = document.querySelectorAll('.number');
-const operatorBtnEls = document.querySelectorAll('.operator');
-const equalBtnEl = document.querySelectorAll('.equals');
-const displayEl = document.querySelector('.display');
+// render is updating the textConetnt of display
+// i believe this is the iterator to make display work
 
 const render = () => {
-// render is updating the textConetnt of display
   displayEl.textContent = result
-}
+};
 
 const updateResult = () => {
   result = num1
   render()
-}
+};
 
 const updateNumbers = (event) => {
-  console.log(event.target.textContent)
-if(!operator){
-  if(!num1){
-    num1 = event.target.textContent
-  } else{
-    num1 += event.target.textContent
-  }
-}else {
-  if (!num2){
-    num2 = event.target.textContent
-  }else {
-    num2 += event.target.textContent
-  }
-}  
-updateResult()
+if(num1 && operator){
+  num2 += event.target.textContent;
+  result = num2
+} else{
+  num1 += event.target.textContent;
+  result = num1
 }
+render()
+};
+//console.log(event.target.textContent)
+//console.log(num1, num2)
 
-// console.log(`num1: ${num1}`, `num2: ${num2}`)
 const updateOperator = (event) => {
-  console.log(event.target.textContent)
   operator = event.target.textContent
-  event.target.style.border = 'solid white'
-} 
+  if (operator === 'C'){
+    num1 = '';
+    num2 = '';
+    operator = '';
+    result = 0;
+    render()
+  }
+};
+
+const resetCalc = () => {
+  num1 = '';
+  num2 = '';
+  operator = '';
+  result = 0;
+}
 
 
 const updateEqual = (event) => {
-  console.log(event.target.textContent)
-  // if(!equal)
-
+  if(operator === '+'){
+    result = Number(num1) + Number(num2)
+  }if (operator === '-'){
+    result = Number(num1) - Number(num2)
+  }if (operator === '*'){
+    result = Number(num1) * Number(num2)
+  }if (operator === '/'){
+    result = Number(num1) / Number(num2)
+  }
+  render()
 }
-
-
-  // if I have the number (text content) -> how do I update state?
-  // check what the status of operator is if it is undefined -> update num1 otherwise update num2
-  // num1 = the current?
-
-
-  // if(operator = '+'){
-  //   num1 + num2 
-  // } else if (operator = '-'){ 
-  //   num1 - num2
-  // } else if (operator = '*'){
-  //   num1 * num2
-  // } else if (operator = '/'){
-  //   num1 / num2
-  // }
-
-  // to take the cirrent number(num1) -> update result
-
-
 
 numberBtnEls.forEach((numBtnEl) =>{
   numBtnEl.addEventListener('click', updateNumbers)
@@ -87,13 +82,12 @@ operatorBtnEls.forEach((opBtnEl) => {
   opBtnEl.addEventListener('click', updateOperator)
 })
 
-equalBtnEl.forEach((eqBtnEl) => {
-  eqBtnEl.addEventListener('click', updateEqual)
-})
+equalBtnEl.addEventListener('click', updateEqual);
 
 
 render()
 
 
-  
-  
+ 
+
+  // Cited: Collaberated with Anitra Paul, Samantha Beltran, Travis Neto, and assistance from Joshua Miller during Support sessions. 
